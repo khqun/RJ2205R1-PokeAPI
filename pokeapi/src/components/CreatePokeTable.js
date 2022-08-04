a32373629cd10f9dd476fadf73db8440835d5bf0
hzat <hzathzat@gmail.com>
2022-08-04 21:11:49 +0700

    poke-update-button

--- pokeapi/src/components/CreatePokeTable.js
+++ pokeapi/src/components/CreatePokeTable.js
@@ -1,13 +1,24 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 function CreatePokeTable() {
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
   const [pokeList, setPokeList] = useState([]);
+  const [previousUrl, setPreviousUrl] = useState();
+  const [nowUrl, setNowUrl] = useState('https://pokeapi.co/api/v2/pokemon?limit=100&offset=0');
+  const [nextUrl, setNextUrl] = useState();
+  const goToNextPage = () => {
+    setNowUrl(nextUrl)
+  }
+  const goToPreviousPage = () => {
+    setNowUrl(previousUrl)
+  }
   useEffect(() => {
-    axios.get('https://pokeapi.co/api/v2/pokemon?limit=100&offset=0')
+    axios.get(nowUrl)
       .then((res) => {
         let pokes = res.data.results;
-        setPokeList(pokes)
+        setPokeList(pokes);
+        setNextUrl(res.data.next)
+        setPreviousUrl(res.data.previous)
       })
       .catch(err => {
         throw err
@@ -15,7 +26,7 @@ function CreatePokeTable() {
       .finally(() => [
         setLoading(false)
       ])
-  }, [])
+  })
   if (loading) {
     return (
       <div>
@@ -24,26 +35,32 @@ function CreatePokeTable() {
     )
   }
   return (
-    <div className='poke-table'>
-      <div className='container-fluid'>
-        <table className="table table-striped table-success ">
-          <thead>
-            <tr className="table-dark">
-              <th scope="col">Numbers</th>
-              <th scope="col">Name</th>
-              <th scope="col">URL</th>
-            </tr>
-          </thead>
-          <tbody>
-            {pokeList.map((poke, index) => [
-              <tr key={index}>
-                <th scope="col">{index+1}</th>
-                <td>{poke.name}</td>
-                <td><a href={poke.url} target="_blank" rel="noreferrer">{poke.url}</a></td>
+    <div>
+      <div className='container-fluid d-flex'>
+        <button className={`btn btn-danger `} onClick={goToPreviousPage} disabled={previousUrl ? false : true}>Previous</button>
+        <button className='btn btn-success' onClick={goToNextPage}  disabled={nextUrl ? false : true}>Next</button>
+      </div>
+      <div className='poke-table'>
+        <div className='container-fluid'>
+          <table className="table table-striped table-success ">
+            <thead>
+              <tr className="table-dark">
+                <th scope="col">ID</th>
+                <th scope="col">Name</th>
+                <th scope="col">URL</th>
               </tr>
-            ])}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {pokeList.map((poke, index) => [
+                <tr key={index + 1}>
+                  <th scope="col">{index + 1}</th>
+                  <td>{poke.name}</td>
+                  <td><a href={poke.url} target="_blank" rel="noreferrer">{poke.url}</a></td>
+                </tr>
+              ])}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   )
--- pokeapi/src/index.js
+++ pokeapi/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import CreatePokeTable from './components/CreatePokeTable';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -10,7 +9,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <CreatePokeTable />
-    <App/>
   </React.StrictMode>
 );
 
